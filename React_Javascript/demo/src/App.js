b719ba0d68e1cc8dfd5a470d2d8c3d47b2da249a
Akanksha312002 <ghorpadeakanksha31@gmail.com>
2025-02-03 20:24:28 +0530

    College_Website

---
+++ React_Javascript/demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Facilities from './pages/Facilities';
+
+function App() {
+  return (
+    <div >
+      <Facilities/>
+    </div>
+  );
+}
+
+export default App;
